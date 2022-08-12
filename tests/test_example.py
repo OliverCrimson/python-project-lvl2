0cf7610ebbe3768dab257eed8f7c62cc7c58291c
MonkeyBusiness <crimson.oliver@gmail.com>
2022-08-12 21:19:25 +0300

    import in tests fixed

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,14 +1,17 @@
 import pathlib
 
 import gendiff.flat_differ
-from gendiff.flat_differ import json_to_python, generate_diff, forming
+
 
 import os
 
+from gendiff.convertation import convert
+
+
 def testing_flat():
     if type(gendiff.flat_differ.generate_diff(
-            json_to_python(os.path.join(os.getcwd(), 'file_one.json')),
-            json_to_python(os.path.join(os.getcwd(), 'file_two.json')))) != str:
+            convert(os.path.join(os.getcwd(), 'file_one.json')),
+            convert(os.path.join(os.getcwd(), 'file_two.json')))) != str:
         raise Exception('asd')
     else:
         pass
