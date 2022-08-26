cf11e2c19b013469f8dc29c26b4f30c683d4c0f4
MonkeyBusiness <crimson.oliver@gmail.com>
2022-08-26 13:16:22 +0300

    linter fix

--- gendiff/sample.py
+++ gendiff/sample.py
@@ -1,13 +1,13 @@
-from gendiff.convertation import convert
-from gendiff.flat_differ import generate_diff
-import pprint
-import 
-first_file = convert('sample_one.json')
-second_file = convert('sample_two.json')
-x = generate_diff(first_file, second_file)
-
-# pprint.pp(first_file)
-# pprint.pp(second_file)
-
-for i in first_file.values():
-    print(i)
+# from gendiff.convertation import convert
+# from gendiff.flat_differ import generate_diff
+# import pprint
+# import
+# first_file = convert('sample_one.json')
+# second_file = convert('sample_two.json')
+# x = generate_diff(first_file, second_file)
+#
+# # pprint.pp(first_file)
+# # pprint.pp(second_file)
+#
+# for i in first_file.values():
+#     print(i)
